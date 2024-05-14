020f43cda74b898ef4e7f1798d17e9efe642caed
tangshaoliang <1005191844@qq.com>
2024-05-14 11:44:33 +0800

    事实证明图片像素整数化，对图片翻转时位置微微跳变的bug作用，已关闭computeSize

--- js/util/computeSize.js
+++ js/util/computeSize.js
@@ -1,15 +1,15 @@
 // 通过root，在页面初始化以及页面resize时，计算卡牌宽高，以使得卡牌尺寸为整数px
 function computeSize() {
+  const root = document.documentElement;
+  const winW = window.innerWidth;
+  const winH = window.innerHeight;
   // 如果屏幕过小
   if (winW < 300 / 0.26 || winH < 518 / 0.37) {
     // 则需要重新调整卡牌尺寸
-    const root = document.documentElement;
-    const winW = window.innerWidth;
-    const winH = window.innerHeight;
     // 卡牌是img，只要设置宽/高，另一项尺寸是会自动调整的
     // 如果为竖屏
-    let cardW, cardH;
-    if (winH < winW) {
+    var cardW, cardH;
+    if (winH > winW) {
       cardW = Math.floor(winW * 0.26);
       cardH = Math.floor(cardW * 518 / 300);
     }
@@ -20,4 +20,10 @@ function computeSize() {
     root.style.setProperty('--card-width', cardW + 'px');
     root.style.setProperty('--card-height', cardH + 'px');
   }
+  // console.log(winW);
+  // console.log(cardW);
+  // console.log(root.style.getPropertyPriority('--card-width'));
+  // console.log(winH);
+  // console.log(cardH);
+  // console.log(root.style.getPropertyPriority('--card-height'));
 }
