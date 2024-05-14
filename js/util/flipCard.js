a9d4916ab11b149b90225230452af17fe2783e1c
tangshaoliang <1005191844@qq.com>
2024-05-14 18:57:45 +0800

    我发现整数化又有效了，我准备试试

--- js/util/flipCard.js
+++ js/util/flipCard.js
@@ -5,12 +5,16 @@ function flipCard(card, index) {
   placeholder.classList.add('flipped');
   card.classList.remove('flipable');
   card.classList.add('flipped');
+
+  // 展示卡牌信息详情
   card.addEventListener('click', () => { this.action.showCardInfo(card) });
+
+  // 卡牌垂直于桌面时，切换卡牌图片、名称
   setTimeout(() => {
     // 设置卡牌信息
     card.info = { ...card.info, ...this.cardInfo[this.cardIdList[index]] };
     // 设置卡牌图片
-    card.src = card.info.imgSrc;
+    // card.src = card.info.imgSrc;
     // 设置卡牌名称
     card.before.innerText = card.info.romanNumeral;
     // 设置卡牌英文名称
