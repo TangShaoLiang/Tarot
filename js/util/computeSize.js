2796d1c0a8e37b0a358d242e2b92350587703416
tangshaoliang <1005191844@qq.com>
2024-05-14 09:19:28 +0800

    compute函数准备完成，准备实现图片尺寸的整数化

--- js/util/computeSize.js
+++ js/util/computeSize.js
@@ -1,18 +1,23 @@
 // 通过root，在页面初始化以及页面resize时，计算卡牌宽高，以使得卡牌尺寸为整数px
 function computeSize() {
-  const root = document.documentElement;
-  const winW = window.innerWidth;
-  const winH = window.innerHeight;
   // 如果屏幕过小
   if (winW < 300 / 0.26 || winH < 518 / 0.37) {
     // 则需要重新调整卡牌尺寸
+    const root = document.documentElement;
+    const winW = window.innerWidth;
+    const winH = window.innerHeight;
     // 卡牌是img，只要设置宽/高，另一项尺寸是会自动调整的
     // 如果为竖屏
-    if (winH > winW) {
-      root.style.setProperty('--card-height', Math.floor(winH * 0.37) + 'px');
+    let cardW, cardH;
+    if (winH < winW) {
+      cardW = Math.floor(winW * 0.26);
+      cardH = Math.floor(cardW * 518 / 300);
     }
     else {
-      root.style.setProperty('--card-width', Math.floor(winW * 0.26) + 'px');
+      cardH = Math.floor(winH * 0.37);
+      cardW = Math.floor(cardH * 300 / 518);
     }
+    root.style.setProperty('--card-width', cardW + 'px');
+    root.style.setProperty('--card-height', cardH + 'px');
   }
 }
